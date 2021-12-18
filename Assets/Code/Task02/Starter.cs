277f8bcc2239040062d28e2394438e9396fbc85a
Maksim.S <maxsic85@mail.ru>
2021-12-19 03:16:18 +0500

    Task 02
    ref

--- Assets/Code/Task02/Starter.cs
+++ Assets/Code/Task02/Starter.cs
@@ -7,17 +7,23 @@ namespace Asteroids
     public class Starter : MonoBehaviour
     {
         IShipFabric shipFabric;
+        IBulletFabrick bulletFabrick;
         ShipModel shipModel;
         public Ship Ship;
         private Camera _camera;
+        BarrelModel barrelModel;
+        [SerializeField] private Rigidbody2D _rocket;
+        [SerializeField] private Transform _startPosition;
         // Start is called before the first frame update
         void Start()
         {
             _camera = Camera.main;
             shipModel = new ShipModel(50, 40, 500);
             shipFabric = new ShipFabric();
+            bulletFabrick = new BarrelFabric();
             Ship = shipFabric.CreatePlayer(shipModel);
-
+         //   _startPosition = Ship._shipView.transform;
+            barrelModel = new BarrelModel(_startPosition, 50, 10);
             shipFabric.CreateEnemy(shipModel);
         }
 
@@ -26,6 +32,8 @@ namespace Asteroids
         {
             InputHandler();
             Ship._shipView.UpdateUI(Ship);
+
+          
         }
 
         private void InputHandler()
@@ -44,6 +52,15 @@ namespace Asteroids
             if (Input.GetKeyUp(KeyCode.LeftShift))
             {
                 Ship.RemoveAcceleration();
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                _startPosition = Ship._shipView.transform;
+                var rocket= bulletFabrick.CreateBarrel(new BarrelModel(_startPosition, 100,100));
+                //  rocket.Move(100,1,Time.deltaTime);
+                //var temAmmunition = Instantiate(_rocket, barrelModel.startPosition.position,
+                //_startPosition.rotation);
+                rocket.rigidbody2D.AddForce(_startPosition.up * barrelModel.Force);
             }
         }
     }
